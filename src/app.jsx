da410a6358682ebcdde5e4236057cb4d69431e7b
abbymuir47 <amuir4@byu.edu>
2024-11-06 16:17:36 -0700

    got react working with my header and footer displayed, has a placeholder for app components

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <><header>
+          <h1>Abby Muir<sup>&reg;</sup></h1>
+          <nav>
+              <ul>
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="projects.html">Projects</a></li>
+                  <li><a href="profile.html">User Profile</a></li>
+              </ul>
+          </nav>
+      </header>
+      <main>App components go here</main>
+      <footer>
+        <hr />
+        <span class="text-reset">Abby Muir</span>
+        <br />
+        <a href="https://github.com/abbymuir47/amuir-startup260">GitHub</a>
+    </footer></>
+  );
 }
