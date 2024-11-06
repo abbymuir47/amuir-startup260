38cb255e706dbd6e494e0387655a087fbc7e6e15
abbymuir47 <amuir4@byu.edu>
2024-11-06 16:47:02 -0700

    router is almost working to navigate between all of the pages of my navbar at the top of the screen

--- src/app.jsx
+++ src/app.jsx
@@ -3,30 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from './home/home';
 import { About } from './about/about';
 import { Profile } from './profile/profile';
 import { Projects } from './projects/projects';
-import { Chat } from './chat/chat';
 
 export default function App() {
   return (
-    <><header>
-          <h1>Abby Muir<sup>&reg;</sup></h1>
-          <nav>
-              <ul>
-                  <li><a href="index.html">Home</a></li>
-                  <li><a href="about.html">About</a></li>
-                  <li><a href="projects.html">Projects</a></li>
-                  <li><a href="profile.html">User Profile</a></li>
-              </ul>
-          </nav>
-      </header>
-      <main>App components go here</main>
-      <footer>
-        <hr />
-        <span class="text-reset">Abby Muir</span>
-        <br />
-        <a href="https://github.com/abbymuir47/amuir-startup260">GitHub</a>
-    </footer></>
+    <BrowserRouter>
+        <header>
+            <h1>Abby Muir<sup>&reg;</sup></h1>
+            <nav>
+                <ul>
+                    <li><NavLink className='nav-link' to='home'>Home</NavLink></li>
+                    <li><NavLink className='nav-link' to='about'>About</NavLink></li>
+                    <li><NavLink className='nav-link' to='projects'>Projects</NavLink></li>
+                    <li><NavLink className='nav-link' to='profile'>User Profile</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+        <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+        <footer>
+            <hr />
+            <span class="text-reset">Abby Muir</span>
+            <br />
+            <a href="https://github.com/abbymuir47/amuir-startup260">GitHub</a>
+        </footer>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
