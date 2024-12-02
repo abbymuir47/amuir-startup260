4e33e6459f359cf3d593729cfaeb98bdd817a0dd
abbymuir47 <amuir4@byu.edu>
2024-12-02 11:52:09 -0700

    cleaning up startup react, removed some unnecessary files and added styling

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export function Chat(props) {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <p1>chat placeholder</p1>
+    <main className='container-fluid text-center'>
+      <h1>My Chats:</h1>
+      <p>Here, will get and display previous chats using database data.</p>
+      <p>Can also use Websocket to display messages and responses in real time</p>
+      <textarea id="inputText" class="content is-medium textarea has-fixed-size textarea is-hovered textarea is-info" placeholder="Enter message here" rows="10"></textarea>
+      <form method="get" action="chat.html">
+        <button type="submit">Send</button>
+      </form>
     </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -7,9 +7,9 @@ import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
   return (
-    <main className='container-fluid bg-secondary text-center'>
+    <main className='container-fluid text-center'>
       <div>
-        {authState !== AuthState.Unknown && <h1>Welcome to Abby's Profile</h1>}
+        {authState !== AuthState.Unknown && <h1>User Login</h1>}
         {authState === AuthState.Authenticated && (
           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
         )}
